2248ba06190229b80982c2c1c753328c1b2e847e
Adam Smith <adamsmith@youlum.com>
2015-05-25 18:01:38 -0700

    Created the Public API

---
+++ src/pre-compiled-tester/public-api.js
@@ -0,0 +1,33 @@
+  /**
+   * ---------------------------------------------------
+   * Global Variable (aIV)
+   * ---------------------------------------------------
+   * @desc Holds the public API for aIV's apps, tools, and libraries.
+   * @struct
+   * @global
+   */
+  window.aIV = window.aIV || {};
+
+  /**
+   * ---------------------------------------------------
+   * Global Method (aIV.tester.runTests)
+   * ---------------------------------------------------
+   * @desc Runs the tests given to aIV.tester.
+   * @param {number=} startIndex - The index of the test in the tests array to
+   *   start with (runs the test and skips all tests before it).
+   * @param {number=} endIndex - The index of the test in the tests array to end
+   *   with (runs the test and skips all tests after it).
+   * @global
+   */
+  aIV.tester.runTests = testerModuleAPI.runTests;
+
+  /**
+   * ---------------------------------------------------
+   * Global Method (aIV.tester.addTest)
+   * ---------------------------------------------------
+   * @desc Adds a new test to the aIV.tester.tests array.
+   * @param {string} title - The title for the new test.
+   * @param {function} testFunc - The actual test.
+   * @global
+   */
+  aIV.tester.addTest = testerModuleAPI.addTest;
