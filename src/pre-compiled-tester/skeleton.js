da074f111c1512596bb49f0484f1842e7154b832
Adam Smith <adamsmith@youlum.com>
2015-05-25 18:01:25 -0700

    Built the tester's Skeleton

---
+++ src/pre-compiled-tester/skeleton.js
@@ -0,0 +1,63 @@
+/**
+ * -----------------------------------------------------------------------------
+ * Algorithm IV Quick Tester (0.0.1)
+ * -----------------------------------------------------------------------------
+ * @file A JavaScript module used to run quick unit tests with minimal overhead.
+ * @module aIVTester
+ * @version 0.0.1
+ * @author Adam Smith {@link adamsmith@youlum.com}
+ * @copyright 2015 Adam A Smith [github.com/imaginate]{@link https://github.com/imaginate}
+ * @license The Apache License [algorithmiv.com/docs/license]{@link http://algorithmiv.com/docs/license}
+ * @desc Details about the used annotations:
+ * - [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ * - [JSDoc3]{@link http://usejsdoc.org/}
+ */
+
+////////////////////////////////////////////////////////////////////////////////
+// The Public API
+////////////////////////////////////////////////////////////////////////////////
+
+;(function setupTheTesterPublicAPI(testerModuleAPI, undefined) {
+  "use strict";
+
+/* -----------------------------------------------------------------------------
+ * The Public API (public-api.js)
+ * -------------------------------------------------------------------------- */
+// insert-public-api
+
+})(
+
+////////////////////////////////////////////////////////////////////////////////
+// The Quick Tester Module
+////////////////////////////////////////////////////////////////////////////////
+
+(function setupTheTesterModule(undefined) {
+  "use strict"; 
+
+/* -----------------------------------------------------------------------------
+ * The Tests Module API (module/api.js)
+ * -------------------------------------------------------------------------- */
+// insert-api
+
+/* -----------------------------------------------------------------------------
+ * The Module Helpers (module/helpers.js)
+ * -------------------------------------------------------------------------- */
+// insert-helpers
+
+/* -----------------------------------------------------------------------------
+ * The Module Variables (module/vars.js)
+ * -------------------------------------------------------------------------- */
+// insert-vars
+
+/* -----------------------------------------------------------------------------
+ * The Module Methods (module/methods.js)
+ * -------------------------------------------------------------------------- */
+// insert-methods
+
+////////////////////////////////////////////////////////////////////////////////
+// The Quick Tester Module End
+////////////////////////////////////////////////////////////////////////////////
+
+  return testerModuleAPI;
+
+})());
